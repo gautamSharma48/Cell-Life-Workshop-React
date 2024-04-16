5fd5ccadc4da1f92f9fde6cf32185f386a2b56df
lhtgautamsharma <95333485+Gautam-Sharma-leewayhertz@users.noreply.github.com>
2024-04-16 18:08:01 +0530

    removed commented code

--- src/reducer/gridReducer.js
+++ src/reducer/gridReducer.js
@@ -23,29 +23,3 @@ export default function grid(state = initialState, action) {
       return state;
   }
 }
-
-// const gridReducer = createSlice({
-//   name: "grid",
-//   initialState,
-//   reducers: {
-//     add: (state=initialState, action) => {
-//       console.log(state);
-//       const data = [
-//         ...state,
-//         { id: action.payload.id, name: action.payload.name },
-//       ];
-//       localStorage.setItem("grid", JSON.stringify(data));
-//       return data;
-//     },
-//     update: (state = initialState, action) => {
-//       const currentIndex = state.find(
-//         (item) => item.id.toString() === action.payload.id.toString()
-//       );
-//       console.log(currentIndex);
-//     },
-//   },
-// });
-
-// export const { add, update } = gridReducer.actions;
-
-// export default gridReducer.reducer;
